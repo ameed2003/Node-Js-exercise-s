cfab9845808f7b652f0fabf87106b989dabb6556
ameed2003 <ameedhalabi2003@gmail.com>
2025-10-30 14:57:22 +0200

    Fix formatting and comments in ex3.js

--- ex3.js
+++ ex3.js
@@ -1,4 +1,6 @@
-//EX #3 :
+// Ameed Halabi & Lotem Sharir
+
+// EX #3 :
 
 let arr = [10, 0, 2, 0];
 let count = 0; // Counter for the number zero .
@@ -10,3 +12,4 @@ for (
   count += Number(arr[i] == 0); // If using boolean terms to check if the number is zero if true add one to the count else add zero .
 }
 console.log(count);
+
